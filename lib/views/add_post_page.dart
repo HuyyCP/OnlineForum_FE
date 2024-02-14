579a94d3bdcc93eca049c933c435969fde2d46d1
huyycp <baohuyvongoc1@gmail.com>
2024-02-14 16:26:22 +0700

    fix overflow add post + update appicon

--- lib/views/add_post_page.dart
+++ lib/views/add_post_page.dart
@@ -29,8 +29,8 @@ class _AddPostPageState extends State<AddPostPage> {
         title: const Text("Add post"),
         backgroundColor: Colors.grey[300],
       ),
+      // resizeToAvoidBottomInset: false,
       body : Container(
-        margin: const EdgeInsets.all(10),
         child: FutureBuilder(
           future: SubjectAPIService.getSubject(),
           builder: (context, snapshot) {
@@ -39,115 +39,119 @@ class _AddPostPageState extends State<AddPostPage> {
                 margin: const EdgeInsets.all(10),
                 child: Form(
                   key: _formKey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      DropdownButtonFormField(
-                        // key: _subjectKey,
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        validator: validateSelectionItem,
-                        decoration: const InputDecoration(
-                          labelText: "Subject",
-                        ),
-                        hint: const Text("Select an option"),
-                        items: snapshot.data!.map(
-                          (item) => DropdownMenuItem(
-                            value: item.idsubject, 
-                            child: Text(item.subjectname))
-                          ).toList(),
-                        onChanged: (value) {
-                          setState(() {
-                            _subsubjectKey.currentState?.reset();
-                            selectedSubject = snapshot.data!.firstWhere((element) => element.idsubject == value);
-                          });
-                        }
-                      ),
-                      DropdownButtonFormField(
-                        key: _subsubjectKey,
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        validator:  validateSelectionItem,
-                        decoration: const InputDecoration(
-                          labelText: "Sub subject",
-                        ),
-                        hint: const Text("Select an option"),
-                        items: selectedSubject == null ? List.empty() : snapshot.data!.firstWhere((element) => element.idsubject == selectedSubject!.idsubject).subsubjects!.map(
-                          (item) => DropdownMenuItem(
-                            value: item.idsubject, 
-                            child: Text(item.subjectname))
-                          ).toList(), 
-                        onChanged: (value) {
-                          selectedSubsubject = value.toString();
-                          setState(() { });
-                        }
-                      ),
-                      TextFormField(
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        validator: validateEmpty,
-                        controller: titleController,
-                        decoration: const InputDecoration(
-                          labelText: "Title"
-                        ),
-                      ),
-                      TextFormField(
-                        autovalidateMode: AutovalidateMode.onUserInteraction,
-                        validator: validateEmpty,
-                        maxLines: 10,
-                        controller: contentController,
-                        decoration: const InputDecoration(
-                          labelText: "Content"
-                        ),
-                      ),
-                      Container(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)
-                            )
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        DropdownButtonFormField(
+                          // key: _subjectKey,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: validateSelectionItem,
+                          decoration: const InputDecoration(
+                            labelText: "Subject",
                           ),
-                          onPressed: () async {
-                            if(_formKey.currentState!.validate()) {
-                              String idsubject = selectedSubsubject!;
-                              String title = titleController.text;
-                              String content = contentController.text;
-                              if(await PostAPIService.addPost(idsubject, title, content)) {
-                                showDialog(
-                                  context: context,
-                                  builder: (context) {
-                                    return AlertDialog(
-                                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                                      contentPadding: const EdgeInsets.all(20),
-                                      content: const Text(
-                                        "Add post successfully",
-                                        textAlign: TextAlign.center,
-                                      ),
-                                    );
-                                  }
-                                );
-                                _formKey.currentState!.reset();
-                                titleController.text = "";
-                                contentController.text = "";
-                              } else {
-                                showDialog(
-                                  context: context,
-                                  builder: (context) {
-                                    return AlertDialog(
-                                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-                                      contentPadding: const EdgeInsets.all(20),
-                                      content: const Text(
-                                        "Add post failed",
-                                        textAlign: TextAlign.center,
-                                      ),
-                                    );
-                                  }
-                                );
-                              }
-                            }
-                          }, 
-                          child: const Text("Add post")
+                          hint: const Text("Select an option"),
+                          items: snapshot.data!.map(
+                            (item) => DropdownMenuItem(
+                              value: item.idsubject, 
+                              child: Text(item.subjectname))
+                            ).toList(),
+                          onChanged: (value) {
+                            setState(() {
+                              _subsubjectKey.currentState?.reset();
+                              selectedSubject = snapshot.data!.firstWhere((element) => element.idsubject == value);
+                            });
+                          }
                         ),
-                      )
-                    ],
+                        DropdownButtonFormField(
+                          key: _subsubjectKey,
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator:  validateSelectionItem,
+                          decoration: const InputDecoration(
+                            labelText: "Sub subject",
+                          ),
+                          hint: const Text("Select an option"),
+                          items: selectedSubject == null ? List.empty() : snapshot.data!.firstWhere((element) => element.idsubject == selectedSubject!.idsubject).subsubjects!.map(
+                            (item) => DropdownMenuItem(
+                              value: item.idsubject, 
+                              child: Text(item.subjectname))
+                            ).toList(), 
+                          onChanged: (value) {
+                            selectedSubsubject = value.toString();
+                            setState(() { });
+                          }
+                        ),
+                        TextFormField(
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: validateEmpty,
+                          controller: titleController,
+                          maxLines: 2,
+                          decoration: const InputDecoration(
+                            labelText: "Title"
+                          ),
+                        ),
+                        TextFormField(
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          validator: validateEmpty,
+                          maxLines: 16,
+                          controller: contentController,
+                          decoration: const InputDecoration(
+                            labelText: "Content"
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(top: 20),
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)
+                              )
+                            ),
+                            onPressed: () async {
+                              if(_formKey.currentState!.validate()) {
+                                String idsubject = selectedSubsubject!;
+                                String title = titleController.text;
+                                String content = contentController.text;
+                                if(await PostAPIService.addPost(idsubject, title, content)) {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                                        contentPadding: const EdgeInsets.all(20),
+                                        content: const Text(
+                                          "Add post successfully",
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      );
+                                    }
+                                  );
+                                  _formKey.currentState!.reset();
+                                  titleController.text = "";
+                                  contentController.text = "";
+                                } else {
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) {
+                                      return AlertDialog(
+                                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                                        contentPadding: const EdgeInsets.all(20),
+                                        content: const Text(
+                                          "Add post failed",
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      );
+                                    }
+                                  );
+                                }
+                              }
+                            }, 
+                            child: const Text("Add post")
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               );
